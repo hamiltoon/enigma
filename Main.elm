6f8bc0b3e06efef987e28b4482c46c646e430523
Hamiltoon <john@publit.se>
2016-12-02 15:37:55 +0100

    Adds Main.elm

---
+++ Main.elm
@@ -0,0 +1,43 @@
+
+import Html exposing (Html, div, text, program)
+
+type alias Model =
+    String
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( "Hello", Cmd.none )
+
+type Msg
+	= NoOp
+
+type Msg
+	= Expand
+	| Collapse
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text model ]
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+main : Program Never Model Msg
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
